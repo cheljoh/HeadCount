3b48898c702ce7b6d11baebda4bf52a5884986ce
Chelsea Johnson <cheljoh@gmail.com>
2016-01-12 20:35:33 -0700

    changed test

--- test/enrollment_repo_test.rb
+++ test/enrollment_repo_test.rb
@@ -12,19 +12,21 @@ class EnrollmentRepositoryTest < Minitest::Test
   #   end
   # end
 
+  def setup
+    @enrollment = EnrollmentRepository.new
+  end
+
   def test_kindergarten_rate_adams
-    e = EnrollmentRepository.new
-    e.load_data({:enrollment => {:kindergarten => "./data/kindergartners in full-day program.csv"}})
-    enrollment = e.find_by_name("adams county 14")
-    rate = enrollment.kindergarten_participation["2009"]
+    @enrollment.load_data({:enrollment => {:kindergarten => "./data/kindergartners in full-day program.csv"}})
+    find_name = @enrollment.find_by_name("adams county 14")
+    rate = find_name.kindergarten_participation["2009"]
     assert_equal "1", rate
   end
 
   def test_kindergarten_rate_academy_20_2007
-    e = EnrollmentRepository.new
-    e.load_data({:enrollment => {:kindergarten => "./data/kindergartners in full-day program.csv"}})
-    enrollment = e.find_by_name("academy 20")
-    rate = enrollment.kindergarten_participation["2007"]
+    @enrollment.load_data({:enrollment => {:kindergarten => "./data/kindergartners in full-day program.csv"}})
+    find_name = @enrollment.find_by_name("academy 20")
+    rate = find_name.kindergarten_participation["2007"]
     assert_equal "0.39159", rate
   end
 end
