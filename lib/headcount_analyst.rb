7926af6723a99e728d447c3af62c9722b4148ada
Chelsea Johnson <cheljoh@gmail.com>
2016-01-13 19:51:09 -0700

    iter 1

--- lib/headcount_analyst.rb
+++ lib/headcount_analyst.rb
@@ -5,33 +5,49 @@ class HeadcountAnalyst
 
   def initialize(district_repository)
     @district_repository = district_repository
-    @district_repository.load_data({:enrollment => {:kindergarten => "./test/fixtures/Kindergartners in full-day program.csv"}})
-  end
+    @district_repository.load_data({:enrollment => {:kindergarten => "./data/Kindergartners in full-day program.csv"}})
+  end                                                               #should this use test fixtures?
 
   def kindergarten_participation_rate_variation(district_name, hash)
-    comparison = hash[:against]
-    district_average = get_enrollment_values(district_name)
-    comparison_average = get_enrollment_values(comparison)
+    comparison_name = hash[:against]
+    district_average = get_average_enrollment_rate(district_name)
+    comparison_average = get_average_enrollment_rate(comparison_name)
     average = district_average/comparison_average
     Truncate.truncate_number(average)
   end
 
-  def get_enrollment_values(value)
-    instance = @district_repository.find_by_name(value)
-    instance_hash = instance.enrollment.kindergarten_participation_by_year
-    instance_values = instance_hash.values
-    average_values = get_average_values(instance_values)
+  def get_average_enrollment_rate(district_name)
+    instance = @district_repository.find_by_name(district_name)
+    instance_values = instance.enrollment.kindergarten_participation_by_year.values
+    average_value = compute_average(instance_values)
   end
 
-  def get_average_values(average_values)
-    average_values.inject(0) do |sum, value|
+  def compute_average(values)
+    values.inject(0) do |sum, value|
       sum + value
-    end/(average_values.count)
+    end/(values.count)
+  end
+
+  def kindergarten_participation_rate_variation_trend(district_name, hash)
+    comparison_name = hash[:against]
+    comparison_average = get_average_enrollment_rate(comparison_name)
+    district_instance = @district_repository.find_by_name(district_name)
+    district_hash = district_instance.enrollment.kindergarten_participation_by_year
+    get_trend_for_each_year(district_hash, comparison_average)
+  end
+
+  def get_trend_for_each_year(district_hash, comparison_average)
+    trends = district_hash.map do |key, value|
+      [key, Truncate.truncate_number(value/comparison_average)]
+    end.to_h
+    trends
   end
 
 end
-# 
+
+
+ #=> {2009 => 0.766, 2010 => 0.566, 2011 => 0.46 }
 # dr = DistrictRepository.new
-# ha = HeadcountAnalyst.new(dr)
-#
-# puts ha.kindergarten_participation_rate_variation('ACADEMY 20', :against => 'COLORADO') # => 0.766
+# @headcount_analyst = HeadcountAnalyst.new(dr)
+# rate = @headcount_analyst.kindergarten_participation_rate_variation_trend('ACADEMY 20', :against => 'COLORADO')
+# puts rate
--- test/district_test.rb
+++ test/district_test.rb
@@ -1,4 +1,5 @@
 require './lib/district'
+require './lib/enrollment'
 require_relative 'test_helper'
 
 class DistrictTest < Minitest::Test
@@ -6,6 +7,12 @@ class DistrictTest < Minitest::Test
   def test_does_it_have_a_name
     district = District.new({:name => "Academy 20"})
     assert_equal "ACADEMY 20", district.name
+  end
+
+  def test_does_it_have_an_enrollment_attribute
+    district = District.new({:name => "Academy 20"})
+    assert district.respond_to?(:enrollment)
+
   end
 
 
