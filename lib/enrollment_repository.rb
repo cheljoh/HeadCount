05e3b2d7126469576925383b104b3df217e6bb97
Chelsea Johnson <cheljoh@gmail.com>
2016-01-14 09:54:35 -0700

    fixed paths

--- lib/enrollment_repository.rb
+++ lib/enrollment_repository.rb
@@ -3,7 +3,7 @@ require_relative 'enrollment'
 
 class EnrollmentRepository
 
-  attr_accessor :enrollment_objects
+  attr_reader :enrollment_objects
 
   def initialize
     @enrollment_objects = {}
@@ -11,37 +11,29 @@ class EnrollmentRepository
 
   def load_data(hash)
     data_path = hash[:enrollment][:kindergarten]#key of  hash
-    data_path2 = hash[:enrollment][:high_school_graduation]
+    if !hash[:enrollment][:high_school_graduation].nil?
+      data_path = hash[:enrollment][:high_school_graduation]
+      graduation_hashes = load_path(data_path)
+    end
 
-
-    participation_hashes = load_kindergarten(data_path)
-    graduation_hashes = load_high_school(data_path2)
-
-    # All hashes are built, make Enrollment objects which require a
-    # {district => {year => rate}} hash to construct
-    # participation_hash.each do |district_name, participation_hash|
-    #   enrollment = Enrollment.new({:name => district_name, :kindergarten_participation => participation_hash}) #inner hash is kindergarten_participation
-    #   enrollment_objects[district_name] = enrollment
-    # end
-    #
-    # graduation_hash.each do |district_name, graduation_hash|
-    #   enrollment = Enrollment.new({:name => district_name, :high_school_graduation_rates => graduation_hash}) #inner hash is kindergarten_participation
-    #   enrollment_objects[district_name] = enrollment
-    # end
+    participation_hashes = load_path(data_path)
 
     participation_hashes.each_key do |district_name|
       participation_hash = participation_hashes[district_name]
-      graduation_hash = graduation_hashes[district_name]
-
-      enrollment = Enrollment.new({:name => district_name, :kindergarten_participation => participation_hash,
-                                   :high_school_graduation_rates => graduation_hash}) #inner hash is kindergarten_participation
+      if !graduation_hashes.nil?
+        graduation_hash = graduation_hashes[district_name]
+        enrollment = Enrollment.new({:name => district_name, :kindergarten_participation => participation_hash,
+                                     :high_school_graduation_rates => graduation_hash})
+      else
+        enrollment = Enrollment.new({:name => district_name, :kindergarten_participation => participation_hash})
+                                   #:high_school_graduation_rates => graduation_hash}) #inner hash is kindergarten_participation
+      end
 
       enrollment_objects[district_name] = enrollment
     end
   end
 
-  def load_kindergarten(data_path)
-    # return participation_hash
+  def load_path(data_path) # return participation_hash
       contents = CSV.open data_path, headers: true, header_converters: :symbol
 
       # First, construct hashes which = {district => {year => rate}}
@@ -54,29 +46,6 @@ class EnrollmentRepository
         year = row[:timeframe].to_i
         rate = row[:data].to_f
         hashes.fetch(district)[year] = rate
-        # require 'pry'
-        # binding.pry
-      end
-
-      return hashes
-  end
-
-  def load_high_school(data_path)
-    # return participation_hash
-      contents = CSV.open data_path, headers: true, header_converters: :symbol
-
-      # First, construct hashes which = {district => {year => rate}}
-      hashes = {}
-      contents.each do |row| #need to do input validation for district and year
-        district = row[:location].upcase
-        if !hashes.has_key?(district)
-          hashes[district] = {}
-        end
-        year = row[:timeframe].to_i
-        rate = row[:data].to_f
-        hashes.fetch(district)[year] = rate
-        # require 'pry'
-        # binding.pry
       end
 
       return hashes
@@ -89,16 +58,16 @@ end
 
 # @enrollment = EnrollmentRepository.new
 # @enrollment.load_data({:enrollment => {:kindergarten => "../test/fixtures/Kindergartners in full-day program.csv"}})
-# puts @enrollment.enrollments
-#puts @enrollment.find_by_name("hello")
-
-er = EnrollmentRepository.new
-er.load_data({:enrollment => { :kindergarten => "../data/Kindergartners in full-day program.csv",
-    :high_school_graduation => "../data/High school graduation rates.csv"}})
-enrollment = er.find_by_name("ACADEMY 20")
-#puts enrollment
-#puts enrollment
-
-puts enrollment.graduation_rate_by_year
-puts enrollment.graduation_rate_in_year(2010)
+# #puts @enrollment.enrollment_objects
+# puts @enrollment.find_by_name("academy 20")
+#
+# er = EnrollmentRepository.new
+# er.load_data({:enrollment => { :kindergarten => "../data/Kindergartners in full-day program.csv",
+#     :high_school_graduation => "../data/High school graduation rates.csv"}})
+# enrollment = er.find_by_name("ACADEMY 20")
+# puts enrollment
+#
+# puts enrollment.kindergarten_participation_by_year
+# puts enrollment.graduation_rate_by_year
+#puts enrollment.graduation_rate_in_year(2010)
 # => <Enrollment>
